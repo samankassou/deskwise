b533e4983980a1f0478e7ac3a2bd3594932f2560
samankassou <samankassoufoulla@gmail.com>
2023-06-17 06:19:45 +0100

    Initial commit

---
+++ resources/js/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -0,0 +1,145 @@
+<script setup>
+import { useTheme } from 'vuetify'
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue'
+import VerticalNavLayout from '@layouts/components/VerticalNavLayout.vue'
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+
+// Components
+import Footer from '@/layouts/components/Footer.vue'
+import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
+import UserProfile from '@/layouts/components/UserProfile.vue'
+
+const vuetifyTheme = useTheme()
+
+const upgradeBanner = computed(() => {
+  return vuetifyTheme.global.name.value === 'light' ? upgradeBannerLight : upgradeBannerDark
+})
+</script>
+
+<template>
+  <VerticalNavLayout>
+    <!-- 👉 navbar -->
+    <template #navbar="{ toggleVerticalOverlayNavActive }">
+      <div class="d-flex h-100 align-center">
+        <!-- 👉 Vertical nav toggle in overlay mode -->
+        <IconBtn
+          class="ms-n3 d-lg-none"
+          @click="toggleVerticalOverlayNavActive(true)"
+        >
+          <VIcon icon="bx-menu" />
+        </IconBtn>
+
+        <VSpacer />
+        <NavbarThemeSwitcher class="me-2" />
+
+        <UserProfile />
+      </div>
+    </template>
+
+    <template #vertical-nav-content>
+      <VerticalNavLink
+        :item="{
+          title: 'Dashboard',
+          icon: 'bx-home',
+          to: '/dashboard',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Account Settings',
+          icon: 'mdi-account-cog-outline',
+          to: '/account-settings',
+        }"
+      />
+
+      <!-- 👉 Pages -->
+      <VerticalNavSectionTitle
+        :item="{
+          heading: 'Pages',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Login',
+          icon: 'bx-log-in',
+          to: '/login',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Register',
+          icon: 'bx-user-plus',
+          to: '/register',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Error',
+          icon: 'bx-info-circle',
+          to: '/no-existence',
+        }"
+      />
+
+      <!-- 👉 User Interface -->
+      <VerticalNavSectionTitle
+        :item="{
+          heading: 'User Interface',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Typography',
+          icon: 'mdi-alpha-t-box-outline',
+          to: '/typography',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Icons',
+          icon: 'bx-show',
+          to: '/icons',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Cards',
+          icon: 'bx-credit-card',
+          to: '/cards',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Tables',
+          icon: 'bx-table',
+          to: '/tables',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: 'Form Layouts',
+          icon: 'mdi-form-select',
+          to: '/form-layouts',
+        }"
+      />
+    </template>
+
+    <!-- 👉 Pages -->
+    <slot />
+
+    <!-- 👉 Footer -->
+    <template #footer>
+      <Footer />
+    </template>
+  </VerticalNavLayout>
+</template>
+
+<style lang="scss" scoped>
+.meta-key {
+  border: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
+  border-radius: 6px;
+  block-size: 1.5625rem;
+  line-height: 1.3125rem;
+  padding-block: 0.125rem;
+  padding-inline: 0.25rem;
+}
+</style>
