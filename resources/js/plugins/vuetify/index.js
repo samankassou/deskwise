9a03222a0ade46f9e096b6b2189ab9953b983a42
samankassou <samankassoufoulla@gmail.com>
2023-06-23 06:14:53 +0100

    wip

--- resources/js/plugins/vuetify/index.js
+++ resources/js/plugins/vuetify/index.js
@@ -1,5 +1,7 @@
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
 import { VBtn } from 'vuetify/components/VBtn'
+import * as directives from 'vuetify/directives'
 import defaults from './defaults'
 import { icons } from './icons'
 import theme from './theme'
@@ -15,4 +17,6 @@ export default createVuetify({
   defaults,
   icons,
   theme,
+  components,
+  directives,
 })
--- resources/js/views/pages/equipment-types/EquipmentTypesTable.vue
+++ resources/js/views/pages/equipment-types/EquipmentTypesTable.vue
@@ -1,8 +1,26 @@
 <script setup>
 const props = defineProps(["equipmentTypes"]);
+const headers = [
+  {
+    title: "#",
+    align: "start",
+    sortable: false,
+    key: "id",
+  },
+  { title: "Name", align: "end", key: "name" },
+  { title: "Slug", align: "end", key: "slug" },
+];
+const itemsPerPage = 5;
 </script>
 
 <template>
+  <VDataTable
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="equipmentTypes"
+    item-value="name"
+    class="elevation-1"
+  ></VDataTable>
   <VTable>
     <thead>
       <tr>
@@ -13,11 +31,15 @@ const props = defineProps(["equipmentTypes"]);
     </thead>
 
     <tbody>
+<<<<<<< Updated upstream
 <<<<<<< HEAD:resources/js/views/pages/equipment-types/EquipmentTypesTable.vue
       <tr v-for="equipmentType in equipmentTypes" :key="type.id">
 =======
       <tr v-for="type in equipmentTypes" :key="type.id">
 >>>>>>> 2f5ee13733f41555965ae7318aaaca9bce58d84e:resources/js/views/pages/equipments-types/EquipmentsTypesTable.vue
+=======
+      <tr v-for="equipmentType in equipmentTypes" :key="equipmentType.id">
+>>>>>>> Stashed changes
         <td>
           {{ equipmentType.id }}
         </td>
